347b93a31b2585690c927a0d6f3f0819c0d3fd3a
hajinstudying <studyinghajin2@gmail.com>
2024-07-15 10:03:32 +0900

    Auto stash before merge of "board" and "origin/board"

--- src/main/java/com/snapshare/web/repository/BoardMapperInterface.java
+++ src/main/java/com/snapshare/web/repository/BoardMapperInterface.java
@@ -2,20 +2,6 @@ package com.snapshare.web.repository;
 
 import java.util.List;
 
-<<<<<<< HEAD
-import com.snapshare.web.vo.BookmarkVo;
-
-public interface BoardMapperInterface {
-	public BookmarkVo getBookmark(int bookmarkId);
-	public List<BookmarkVo> listBookmark();                   
-	public int createBookmark(BookmarkVo boardVo);           
-	public int updateBookmark(BookmarkVo boardVo);
-	public int deleteBookmark(int bno);
-	public int createBookmarkSelectKey(BookmarkVo boardVo);                            
-                                   
-}
-                                               
-=======
 import com.snapshare.web.vo.BoardVo;
 
 
@@ -32,4 +18,3 @@ public interface BoardMapperInterface {
 	// 게시물 삭제
 	public int deleteBoard(int boardId);
 }
->>>>>>> 878221a26f353d20937b20c43f491a2f76e3d0ee

b2f47e83e3f68953df95d5f67d54f51e99af721a
diddPfl <yeri061745@gmail.com>
2024-07-16 19:41:30 +0900

    북마크 js 작업중

--- src/main/java/com/snapshare/web/service/BookmarkService.java
+++ src/main/java/com/snapshare/web/service/BookmarkService.java
@@ -7,10 +7,10 @@ import org.apache.ibatis.annotations.Param;
 import com.snapshare.web.vo.BookmarkVo;
 
 public interface BookmarkService {
-	public BookmarkVo getBookmark(int bookmarkId);
+    public BookmarkVo getBookmark(int bookmarkId);
     public List<BookmarkVo> listBookmark();
     public int insertBookmark(BookmarkVo bookmarkVo);
     public int deleteBookmark(int bookmarkId);
-	public int updateBoardBookmarkCount(@Param("memberId") String memberId, @Param("boardId") int boardId);
+    public int updateBoardBookmarkCount(@Param("memberId") String memberId, @Param("boardId") int boardId);
+    public List<BookmarkVo> getBookmarkedBoards();
 }
-
